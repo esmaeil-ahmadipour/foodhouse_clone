043823ad4741cc2e946c500402dd5e45126d2c79
Esmaeil Ahmadipour <ahmadipour1402@gmail.com>
2026-04-25 19:50:21 +0330

    feat(cart): add button jump animation on item addition

--- src/Components/Layouts/HeaderCardButton.jsx
+++ src/Components/Layouts/HeaderCardButton.jsx
@@ -1,21 +1,39 @@
 import styles from "./HeaderCardButton.module.css";
 import CartIcon from "../Cart/CartIcon.jsx";
-import { useContext } from "react";
+import { useContext, useEffect, useState } from "react";
 import CartContext from "../../store/cart-context.js";
 
 const HeaderCardButton = (props) => {
   const cartCtx = useContext(CartContext);
+  const [isJumping, setIsJumping] = useState(false);
+  const [prevCount, setPrevCount] = useState(0);
+  
   const numberOfCartItems = cartCtx.items.reduce(
     (prevValue, item) => prevValue + item.amount,
     0,
   );
+
+  // Trigger jump when item is added
+  useEffect(() => {
+    if (numberOfCartItems > prevCount && numberOfCartItems > 0) {
+      setIsJumping(true);
+      setTimeout(() => setIsJumping(false), 300);
+    }
+    setPrevCount(numberOfCartItems);
+  }, [numberOfCartItems, prevCount]);
+
   return (
-    <button className={styles.button} onClick={props.onClick}>
+    <button 
+      className={`${styles.button} ${isJumping ? styles.jump : ''}`} 
+      onClick={props.onClick}
+    >
       <span className={styles.icon}>
         <CartIcon />
       </span>
-      <span>Cart</span>
-      <span className={styles.badge}>{numberOfCartItems}</span>
+      <span className={styles.cartText}>Cart</span>
+      <span className={`${styles.badge} ${numberOfCartItems > 0 ? styles.hasItems : ''}`}>
+        {numberOfCartItems}
+      </span>
     </button>
   );
 };
